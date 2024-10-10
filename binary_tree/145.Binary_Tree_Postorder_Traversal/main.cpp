6830e8dac8039913051fa4a99343c042908172e9
Joey Li <ljyhd2024@gmail.com>
2024-10-09 23:37:49 -0600

    145 Binary Tree Postorder Traversal

---
+++ binary_tree/145.Binary_Tree_Postorder_Traversal/main.cpp
@@ -0,0 +1,111 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+
+/* class Solution { */
+/* public: */
+/*     vector<int> preorderTraversal(TreeNode* root) { */
+/*         vector<int> result; */
+/*         queue<TreeNode*> q; */
+/*         if(root) */
+/*             q.push(root); */
+/*  */
+/*         while(!q.empty()) { */
+/*             auto cur = q.front(); */
+/*             result.push_back(cur->val); */
+/*             if(cur->left) */
+/*                 q.push(cur->left); */
+/*             if(cur->right) */
+/*                 q.push(cur->right); */
+/*             q.pop(); */
+/*         } */
+/*  */
+/*         return result; */
+/*     } */
+/* }; */
+
+/* class Solution { */
+/* public: */
+/*     vector<int> preorderTraversal(TreeNode* root) { */
+/*         vector<int> result; */
+/*         stack<TreeNode*> s; */
+/*  */
+/*         if(root) */
+/*             s.push(root); */
+/*  */
+/*         while(!s.empty()) { */
+/*             auto cur = s.top(); */
+/*             s.pop(); */
+/*             result.push_back(cur->val); */
+/*             if(cur->left) */
+/*                 s.push(cur->left); */
+/*             if(cur->right) */
+/*                 s.push(cur->right); */
+/*         } */
+/*  */
+/*         return result; */
+/*     } */
+/* }; */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> s;
+
+        if(root)
+            s.push(root);
+
+        while(!s.empty()) {
+            auto cur = s.top();
+            s.pop();
+            result.push_back(cur->val);
+            if(cur->left)
+                s.push(cur->left);
+            if(cur->right)
+                s.push(cur->right);
+        }
+
+        // just reverse!
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
+
+int main()
+{
+    TreeNode node3(3);
+    TreeNode node2(2, &node3, NULL);
+    TreeNode tree(1, NULL, &node2);
+    auto result = (new Solution())->postorderTraversal(&tree);
+
+    for(auto i: result) {
+        cout<<i<<' ';
+    }
+    cout<<endl;
+}
